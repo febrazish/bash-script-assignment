0a183c47206452fdc79c2a0f2423752c56d6c6ac
Alex Nowak <120033806+febrazish@users.noreply.github.com>
2026-03-23 11:02:28 +0100

    Update bam2bed.sh
    Better version of the script (not using a hardcoded filename)

--- bam2bed.sh
+++ bam2bed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # A script that takes a BAM file and converts it to a BED file
-# It also  filters the contents of the file using regex.
+# It also filters the contents of the file using regex.
 # and creates and activates a conda environment
 
 # Get the command arguments
@@ -10,31 +10,33 @@ outputfolder=$2
 echo "Output folder:" $outputfolder
 
 # Creating a new output directory
-mkdir $outputfolder"/output_dir"
-outputdir="$outputfolder""/output_dir"
+mkdir $outputfolder"/output"
+outputdir="$outputfolder""/output"
 echo "Output directory:" $outputdir
 
 # Conda
-source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
+source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh
 # Creating and activating conda environment with the name 'bam2bed'
 conda create --name bam2bed
 conda activate bam2bed
 
-# Converting input BAM file to a BED file with bedtools
-# and saving it in output directory
-filename="GSM1606197"
-echo $filename
-bedtools bamtobed -i "$bamfile" > "$outputdir/$filename.bed"
-echo "BED file:" "$outputdir/$filename.bed"
+# Converting input BAM file to a BED file with bedtools, removing .bam
+# extension and giving it .bed extension, then saving it in output directory
+filename=$(basename ${bamfile} .bam)
+
+bedtools bamtobed -i $bamfile > "$outputdir/$filename.bed"
+echo "BED filename:" "$outputdir/$filename.bed"
+
+echo "BED file line count:" $(wc -l "$outputdir/$filename.bed")
 
 # Filtering the BED file with regex for all regions from chromosome 1
 # and saving those in another BED file with the suffix _chr1.bed
-grep chr.*1 "$outputdir/$filename.bed" > "$outputdir/$filename""_chr1.bed"
-echo "Chromosome 1 file:" "$outputdir/$filename""_chr1.bed"
+grep -i "chr1" "$outputdir/$filename.bed" > "$outputdir/$filename""_chr1.bed"
+echo "Chromosome 1 BED filename:" "$outputdir/$filename""_chr1.bed"
 
 # Counting the number of lines in the filtered file and storing the count in
 # a new file called bam2bed_number_of_rows.txt
 wc -l "$outputdir/$filename""_chr1.bed" > $outputdir"/bam2bed_number_of_rows.txt"
-echo "Count file:" $outputdir"/bam2bed_number_of_rows.txt"
+echo "Linecount filename:" $outputdir"/bam2bed_number_of_rows.txt"
 
 echo Alex
